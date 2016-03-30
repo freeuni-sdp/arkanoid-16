d27437953e533656ab8c2876bc5d905abf478299
George Mamaladze <gmamaladze@hotmail.com>
2016-03-30 15:46:32 +0200

    Demonstrating how to create a test level for particular feature.

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/TestFrameBuilder.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/TestFrameBuilder.java
@@ -3,39 +3,38 @@ package ge.edu.freeuni.sdp.arkanoid.model;
 import ge.edu.freeuni.sdp.arkanoid.model.geometry.Point;
 import ge.edu.freeuni.sdp.arkanoid.model.geometry.Size;
 
-public class FrameBuilder implements RoomBuilder {
+public class TestFrameBuilder implements RoomBuilder {
+    private Size _size;
 
-    private final Size _size;
-
-    public FrameBuilder(Size size) {
+    public TestFrameBuilder(Size size) {
         _size = size;
     }
 
     public void build(Room room, ScoreCounter scoreCounter) {
-
         int h = _size.getHeight();
         int w = _size.getWidth();
 
-        //NOTE: Bottom is a killer brick
-        Brick bottomBrick = new KillerBrick(
+        //NOTE: Bottom is not a killer brick good for testing
+        Brick bottomBrick = new FrameBrick(
                 new Point(-1, h),
                 new Size(w, 1));
-            room.add(bottomBrick);
+        room.add(bottomBrick);
 
         Brick topBrick = new FrameBrick(
                 new Point(-1, -1),
                 new Size(w, 1));
-            room.add(topBrick);
+        room.add(topBrick);
 
 
         Brick leftBrick = new FrameBrick(
                 new Point(-1, 0),
                 new Size(1, h - 1));
-            room.add(leftBrick);
+        room.add(leftBrick);
 
         Brick rightBrick = new FrameBrick(
                 new Point(w, 0),
                 new Size(1, h - 1));
-            room.add(rightBrick);
+        room.add(rightBrick);
+
     }
 }
