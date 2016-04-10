3179855304f3839cb1675332723c98540a329c3f
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-04-10 23:29:29 +0400

    Moving wall kills paddle #10, #16

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/SinkingBrick.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/SinkingBrick.java
@@ -5,7 +5,7 @@ import ge.edu.freeuni.sdp.arkanoid.model.geometry.Point;
 
 public class SinkingBrick extends MovingBrick {
 
-    private static final long SINKING_DELAY = 500;
+    private static final long SINKING_DELAY = 10000;
 
     public SinkingBrick(Point position, BrickColor color, Capsule capsule) {
         super(position, color, capsule, Direction.DOWN, SINKING_DELAY);
