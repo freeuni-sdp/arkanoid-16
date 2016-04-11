92f7d47bb266830b56c2bd7ffb97ec9a72b4ecd5
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-04-12 02:24:29 +0400

    Clean up code

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/ExpandCapsule.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/ExpandCapsule.java
@@ -13,28 +13,9 @@ public class ExpandCapsule extends Capsule {
         super.interact(other);
         if (other instanceof Paddle) {
             Paddle oldPaddle = (Paddle) other;
-            int oldPaddleWidth = oldPaddle.getShape().getSize().getWidth();
-            int expandedPaddleWidth = getExpandedPaddleWidth();
-            Point newPosition = getNewPosition(other.getPosition(), oldPaddleWidth, expandedPaddleWidth);
-            Paddle newPaddle = new ExpandedPaddle(newPosition);
+            Paddle newPaddle = new ExpandedPaddle(other.getPosition());
             oldPaddle.exchange(newPaddle);
             SoundPlayer.getInstance().play(SoundPlayer.EXTEND);
         }
-    }
-
-    private Point getNewPosition(Point oldPosition, int oldPaddleWidth, int expandedPaddleWidth) {
-        Point newPosition = new Point(oldPosition);
-
-        if (oldPosition.X <= 0) {
-            newPosition.X += (expandedPaddleWidth - oldPaddleWidth) / 2;
-        } else if (oldPosition.X + oldPaddleWidth >= Configuration.getInstance().getSize().getWidth()) {
-            newPosition.X -= (expandedPaddleWidth - oldPaddleWidth) / 2;
-        }
-
-        return newPosition;
-    }
-
-    private int getExpandedPaddleWidth() {
-        return new ExpandedPaddle(null).getShape().getSize().getWidth();
     }
 }
