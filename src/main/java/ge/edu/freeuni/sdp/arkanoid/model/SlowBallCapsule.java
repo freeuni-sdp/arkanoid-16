9ce4abe09f0cf7c8335b551fea08d978f9bd92bb
ninucabas <nbasi13@freeuni.edu.ge>
2016-04-12 20:44:15 +0400

    S capsule and test

---
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/SlowBallCapsule.java
@@ -0,0 +1,14 @@
+package ge.edu.freeuni.sdp.arkanoid.model;
+
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Point;
+
+/**
+ * Created by nino on 12.04.2016.
+ */
+public class SlowBallCapsule extends Capsule {
+
+    SlowBallCapsule(Point position, Room room) {
+        super(position, room);
+    }
+
+}
---
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/TestSCapsule.java
@@ -0,0 +1,14 @@
+package ge.edu.freeuni.sdp.arkanoid.model;
+
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Size;
+
+/**
+ * Created by nino on 12.04.2016.
+ */
+public class TestSCapsule extends TestFrameBuilder{
+
+    public TestSCapsule(Size size) {
+        super(size);
+    }
+
+}
