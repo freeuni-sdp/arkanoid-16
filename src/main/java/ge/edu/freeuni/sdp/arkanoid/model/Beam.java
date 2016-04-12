ffaa05c95434fff71c370f431896712a24573bd6
babdus <babdusi@gmail.com>
2016-04-13 02:47:49 +0400

    Add Beam size demo version

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/Beam.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/Beam.java
@@ -6,10 +6,12 @@ import ge.edu.freeuni.sdp.arkanoid.model.geometry.Rectangle;
 public abstract class Beam extends Gobj<Rectangle> {
 
     private boolean _isAlive;
+    private Room _room;
 
-    Beam(Point position) {
+    Beam(Point position, Room room) {
         super(position);
         _isAlive = true;
+        _room = room;
     }
 
     @Override
@@ -22,8 +24,25 @@ public abstract class Beam extends Gobj<Rectangle> {
     }
 
     @Override
+    public Rectangle getShape() {
+        int height = getHeight();
+        Size size = new Size(1, height);
+        return new Rectangle(getPosition(), size);
+    }
+
+    @Override
     public boolean isAlive() {
         return _isAlive;
+    }
+
+    private int getHeight() {
+        _room.getGobjs().stream().filter(obj -> obj instanceof Brick).forEach(obj -> {
+            Brick brick = (Brick) obj;
+            Point brickPosition = brick.getPosition();
+            //Todo get lowest brick position at piont.Y
+        });
+        //Todo return distance from lowest brick to paddle
+        return 10;
     }
 
 }
