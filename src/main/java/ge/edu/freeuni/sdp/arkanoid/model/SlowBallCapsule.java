1953242e0391f7de3e588ed4749961187e9c6e7b
ninucabas <nbasi13@freeuni.edu.ge>
2016-04-12 20:47:08 +0400

    S capsule and test

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/SlowBallCapsule.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/SlowBallCapsule.java
@@ -1,6 +1,7 @@
 package ge.edu.freeuni.sdp.arkanoid.model;
 
 import ge.edu.freeuni.sdp.arkanoid.model.geometry.Point;
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Speed;
 
 /**
  * Created by nino on 12.04.2016.
@@ -11,4 +12,20 @@ public class SlowBallCapsule extends Capsule {
         super(position, room);
     }
 
+    public void interact(Gobj other) {
+        super.interact(other);
+
+        if (other instanceof Paddle) {
+            //slow down ball
+            _room.getGobjs()
+            .stream()
+            .filter(obj -> obj instanceof Ball)
+            .forEach(obj -> {
+                Ball ball = (Ball) obj;
+                Speed ballSpeed = ball.getSpeed();
+                ballSpeed.setLength(ballSpeed.getLength()/2);
+                ball.setSpeed(ballSpeed);
+            });
+        }
+    }
 }
--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/TestSCapsule.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/TestSCapsule.java
@@ -1,5 +1,6 @@
 package ge.edu.freeuni.sdp.arkanoid.model;
 
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Point;
 import ge.edu.freeuni.sdp.arkanoid.model.geometry.Size;
 
 /**
@@ -9,6 +10,19 @@ public class TestSCapsule extends TestFrameBuilder{
 
     public TestSCapsule(Size size) {
         super(size);
+    }
+
+    public void build(Room room, ScoreCounter scoreCounter) {
+        super.build(room, scoreCounter);
+        int brickDistance = 4;
+        int roomWidth = Configuration.getInstance().getSize().getWidth();
+
+        for (int i = 0; i < roomWidth; i += brickDistance) {
+            Point position = new Point(i, 5);
+            Capsule capsule = new SlowBallCapsule(position, room);
+            Brick current = new NormalBrick(position, BrickColor.Red, capsule);
+            room.add(current);
+        }
     }
 
 }
