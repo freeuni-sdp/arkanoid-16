a3fb686b2223f3673889a4f3357bef15bf5c0856
Babdus <Babdusi@gmail.com>
2016-04-13 08:18:43 +0400

    Add if to fire method, make Beam only if isFirable

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/Paddle.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/Paddle.java
@@ -83,8 +83,10 @@ public class Paddle extends Gobj<Rectangle> {
     }
 
     void fire(Room room) {
-        Beam beam = new Beam(getPosition(), room);
-        room.add(beam);
+        if (_isFirable) {
+            Beam beam = new Beam(getPosition(), room);
+            room.add(beam);
+        }
     }
 
     void lifeLost() {
