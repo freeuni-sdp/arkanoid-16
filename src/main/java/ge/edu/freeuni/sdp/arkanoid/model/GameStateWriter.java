0cdbfe7bcbb3b52a8083b56134bcee4d669da062
Davit-Qobuladze <dkobu11@freeuni.edu.ge>
2016-05-02 22:40:14 +0400

    adds stub interface and class

---
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/GameStateWriter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ge.edu.freeuni.sdp.arkanoid.model;
+
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Size;
+
+/**
+ *
+ * @author dato
+ */
+public interface GameStateWriter {
+    
+    public void saveGameState(Ball ball, Paddle paddle, ScoreCounter scoreCounter, Size size);
+    
+}
