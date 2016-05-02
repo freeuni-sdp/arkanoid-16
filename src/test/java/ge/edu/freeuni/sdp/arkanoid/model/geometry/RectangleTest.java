0221f38e1365222f0d7944a109199cf12691ad56
gosip <gosip12@freeuni.edu.ge>
2016-05-02 13:27:11 +0400

    Rectangle and Point overlap tested. Unused mocked removed.

--- src/test/java/ge/edu/freeuni/sdp/arkanoid/model/geometry/RectangleTest.java
+++ src/test/java/ge/edu/freeuni/sdp/arkanoid/model/geometry/RectangleTest.java
@@ -2,7 +2,6 @@ package ge.edu.freeuni.sdp.arkanoid.model.geometry;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
@@ -10,23 +9,50 @@ import static org.mockito.Mockito.*;
 
 public class RectangleTest {
 
-    @Mock Point point;
-    @Mock Size size;
+    private Size size;
+    private Point position;
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
+        position = new Point(0,0);
+        size = new Size(2,3);
     }
 
     @Test
     public void testBottomRightCorner() {
-        Point upperLeft = new Point(0, 0);
-        Size rectSize = new Size(1, 2);
+        Rectangle target = new Rectangle(position, size);
+        Point bottomRight = target.getBottomRight();
 
-        Rectangle rectangle = new Rectangle(upperLeft, rectSize);
-        Point bottomRight = new Point(upperLeft.X + rectSize.getWidth(), upperLeft.Y + rectSize.getHeight());
-
-        assertEquals(bottomRight, rectangle.getBottomRight());
+        assertEquals(position.X + size.getWidth(), bottomRight.X, 0.0001);
+        assertEquals(position.Y + size.getHeight(), bottomRight.Y, 0.0001);
     }
 
+    @Test
+    public void testOverlapWithPointInside() {
+        Point pointInside = new Point((position.X + size.getWidth())/2, (position.Y + size.getHeight()/2));
+
+        Rectangle target = new Rectangle(position, size);
+
+        assertTrue(target.canOverlap(pointInside));
+    }
+
+    @Test
+    public void testOverlapAtBorder() {
+        Point pointInside = new Point(position.X + size.getWidth(), position.Y + size.getHeight());
+
+        Rectangle target = new Rectangle(position, size);
+
+        assertTrue(target.canOverlap(pointInside));
+    }
+
+
+    @Test
+    public void testNoOverlap() {
+        Point pointInside = new Point(position.X + size.getWidth() + 10, position.Y + size.getHeight() + 10);
+
+        Rectangle target = new Rectangle(position, size);
+
+        assertFalse(target.canOverlap(pointInside));
+    }
 }
