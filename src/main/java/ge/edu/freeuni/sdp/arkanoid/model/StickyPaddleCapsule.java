7b3c6306331fb5c8568a96cf93a4a34dc7560347
Nika Chkhartishvili <chxartishvilin@gmail.com>
2016-05-04 03:31:22 +0400

    Finish StickyPaddleCapsule tests

--- src/main/java/ge/edu/freeuni/sdp/arkanoid/model/StickyPaddleCapsule.java
+++ src/main/java/ge/edu/freeuni/sdp/arkanoid/model/StickyPaddleCapsule.java
@@ -6,7 +6,7 @@ import ge.edu.freeuni.sdp.arkanoid.model.geometry.Speed;
 /**
  * Created by dell on 4/12/16.
  */
-public class StickyPaddleCapsule extends Capsule{
+public class StickyPaddleCapsule extends Capsule {
 
     StickyPaddleCapsule(Point position, Room room) {
         super(position, room);
--- src/test/java/ge/edu/freeuni/sdp/arkanoid/model/StickyPaddleCapsuleTest.java
+++ src/test/java/ge/edu/freeuni/sdp/arkanoid/model/StickyPaddleCapsuleTest.java
@@ -1,11 +1,15 @@
 package ge.edu.freeuni.sdp.arkanoid.model;
 
 import ge.edu.freeuni.sdp.arkanoid.model.geometry.Point;
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Rectangle;
+import ge.edu.freeuni.sdp.arkanoid.model.geometry.Speed;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.exceptions.ExceptionIncludingMockitoWarnings;
 
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
@@ -17,13 +21,14 @@ import static org.mockito.Mockito.verify;
 public class StickyPaddleCapsuleTest {
 
     private TestableStickyPaddleCapsule capsule;
-    @Mock private Point mPoint;
     @Mock private Room mRoom;
+    private Point capsulePoint;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        capsule = new TestableStickyPaddleCapsule(mPoint, mRoom);
+        capsulePoint = new Point(0, 1);
+        capsule = new TestableStickyPaddleCapsule(capsulePoint, mRoom);
         capsule.setReturnPaddle(mock(Paddle.class));
     }
 
@@ -53,6 +58,79 @@ public class StickyPaddleCapsuleTest {
         Gobj mGobj = mock(Gobj.class);
         capsule.interact(mGobj);
         // If interact calls exchange Exception will be thrown
+    }
+
+    @Test
+    public void getShape_returnCorrectShape() throws Exception {
+        Rectangle shape = capsule.getShape();
+        assertEquals(shape.getPosition().X, capsulePoint.X, 0);
+        assertEquals(shape.getPosition().Y, capsulePoint.Y, 0);
+    }
+
+    @Test
+    public void isAlive_interactPaddle_isAliveReturnFalse() throws Exception {
+        Paddle mPaddle = mock(Paddle.class);
+        capsule.interact(mPaddle);
+        assertFalse(capsule.isAlive());
+    }
+
+    @Test
+    public void isAlive_interactFrameBrick_isAliveReturnFalse() throws Exception {
+        FrameBrick mBrick = mock(FrameBrick.class);
+        capsule.interact(mBrick);
+        assertFalse(capsule.isAlive());
+    }
+
+    @Test
+    public void isAlive_interactGobj_isAliveReturnTrue() throws Exception {
+        Gobj mGobj = mock(Gobj.class);
+        capsule.interact(mGobj);
+        assertTrue(capsule.isAlive());
+    }
+
+    @Test
+    public void getPosition_constructorPoint_sameCoordinates() throws Exception {
+        Point pos = capsule.getPosition();
+        assertEquals(capsulePoint.X, pos.X, 0);
+        assertEquals(capsulePoint.Y, pos.Y, 0);
+    }
+
+    @Test
+    public void setPosition_changePosition_changeCoordinates() throws Exception {
+        Point newPoint = new Point(capsulePoint.X + 1, capsulePoint.Y + 1);
+        capsule.setPosition(newPoint);
+        Point res = capsule.getPosition();
+        assertEquals(newPoint.X, res.X, 0);
+        assertEquals(newPoint.Y, res.Y, 0);
+    }
+
+    @Test
+    public void getSpeed_speedNull_sameCoordinates() throws Exception {
+        assertEquals(capsule.getSpeed(), Speed.NULL);
+    }
+
+    @Test
+    public void setSpeed_changeSpeed_getSpeedReturnNewSpeed() throws Exception {
+        Speed newSpeed = new Speed(90);
+        capsule.setSpeed(newSpeed);
+        Speed res = capsule.getSpeed();
+        assertEquals(newSpeed, res);
+    }
+
+    @Test
+    public void release_mustFallAndAddToRoom() throws Exception {
+        Point releasePoint = new Point(2, 2);
+        capsule.release(releasePoint);
+
+        // Assert position
+        assertEquals(capsule.getPosition().X, releasePoint.X, 0);
+        assertEquals(capsule.getPosition().Y, releasePoint.Y, 0);
+
+        // Assert that it's falling
+        assertEquals(capsule.getSpeed().getAngleDegrees(), 90, 0.00001);
+
+        // Assert that it's added to room
+        verify(mRoom, times(1)).add(capsule);
     }
 
 }
